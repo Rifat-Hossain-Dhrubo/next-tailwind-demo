73e2d36778ac996ee45745ca27cfe2b332f09ce9
rashed.alam <rashed.alam@workspaceit.com>
2021-12-01 09:59:55 +0600

    minor

---
+++ pages/campaign/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useState } from 'react';
+import { HiChevronDown, HiChevronUp } from 'react-icons/hi';
+import { MultiSelect } from 'react-multi-select-component';
+import Select, { components } from 'react-select';
+
+const Campaign: NextPage = () => {
+  
+  return (
+    <div className="p-4 mx-auto">
+      zzzzzzzzzz
+    </div>
+  );
+};
+
+export default Campaign;
